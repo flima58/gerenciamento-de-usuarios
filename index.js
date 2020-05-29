959cbc62e2a9461514c6fd115f2eecdd49249066
flima58 <franciscoassislima58@gmail.com>
2020-05-28 21:02:22 -0300

    criando a area odne aparece os usuários cadastrados

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ var fields = document.querySelectorAll('#form-user-create [name]')
 var user = {};
 
 
+function addLine(dataUser){
+        console.log("addLine",dataUser);
 
 
-console.log(user)
+        var tr = document.createElement("tr");
+        
+        tr.innerHTML = `<tr>
+                            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                            <td>${dataUser.name}</td>
+                            <td>${dataUser.email}</td>
+                            <td>${dataUser.admin}</td>
+                            <td>${dataUser.birth}</td>
+                            <td>
+                                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                            </td>
+                        </tr>`;
+
+                        document.getElementById("table-users").appendChild(tr);
+        
+}
+
+//A função do evento, sempre vai receber como parametro o evento para SPA
+document.getElementById("form-user-create").addEventListener("submit",function(event){
+
+    event.preventDefault(); //Para tudo que esse evento disparou
 
 
-document.getElementById("form-user-create").addEventListener("submit",function(){
     fields.forEach(function(field, index){
-
-
         if(field.name == "gender"){
             
             if(field.checked === true){
@@ -19,4 +39,13 @@ document.getElementById("form-user-create").addEventListener("submit",function()
             }
                  
             
-  
+    
+        }else {
+            user[field.name] = field.value;
+        }
+        
+    
+    });
+
+    addLine(user)
+});
